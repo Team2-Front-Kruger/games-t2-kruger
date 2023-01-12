f5f792df22a4b7f05f996088f1723c08bc0f4e93
eddyjav <eddy.j.av@gmail.com>
2023-01-12 11:27:37 -0500

    :video_game: init, clean, vite & config tailwind

---
+++ games-app-v/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn">Bt</button>
+    </div>
+  );
+}
+
+export default App;
