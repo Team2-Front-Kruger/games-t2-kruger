73c5c08598559c30127cff0123d90546d5ef63bc
Luisito <luisitoylag@gmail.com>
2023-01-13 15:06:49 -0500

    Funcionamiento del Navbar

--- games-app-v/src/App.jsx
+++ games-app-v/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import { AppRouter } from "./router/AppRouter";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Navbar></Navbar>
       <AppRouter />
     </BrowserRouter>
   );
