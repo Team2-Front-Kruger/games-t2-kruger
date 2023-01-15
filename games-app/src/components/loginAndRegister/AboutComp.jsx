909c05eeb7882f012a57131d4c818d5d48c3249b
BryanArmando <bryanquisaguano@gmail.com>
2023-01-15 18:53:18 -0500

    ultimos cambios

---
+++ games-app/src/components/loginAndRegister/AboutComp.jsx
@@ -0,0 +1,98 @@
+const AboutComp = () => {
+    return ( 
+        <div>
+            <label className="swap swap-rotate">
+                <input type="checkbox" />
+                <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
+                <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
+            </label>
+
+            <div className="hero-content min-w-full flex-col lg:flex-row w-full justify-around">
+
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#202430]">
+                <figure><img className="card" src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
+                    
+                    <div className="card-body text-white">
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Software Developer</div>
+                    <span className="block mt-1 text-lg leading-tight font-medium text-white hover:underline">Bryan Armando Quisaguano Casa</span>
+
+
+                        <div className="text-center">
+                        <p className="mt-2 text-slate-500">Apasionado por el backend con java eaaaa y loas mejore sparcticas de programcación.</p>
+                        </div>
+
+                        <button className="btn btn-secondary" >Google</button>
+
+                    </div>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#202430]">
+                <figure><img className="card" src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
+                    
+                    <div className="card-body text-white">
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Software Developer</div>
+                    <span className="block mt-1 text-lg leading-tight font-medium text-white hover:underline">Bryan Armando Quisaguano Casa</span>
+
+
+                        <div className="text-center">
+                        <p className="mt-2 text-slate-500">Apasionado por el backend con java eaaaa y loas mejore sparcticas de programcación.</p>
+                        </div>
+
+                        <button className="btn btn-secondary" >Google</button>
+
+                    </div>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#202430]">
+                <figure><img className="card" src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
+                    
+                    <div className="card-body text-white">
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Software Developer</div>
+                    <span className="block mt-1 text-lg leading-tight font-medium text-white hover:underline">Bryan Armando Quisaguano Casa</span>
+
+
+                        <div className="text-center">
+                        <p className="mt-2 text-slate-500">Apasionado por el backend con java eaaaa y loas mejore sparcticas de programcación.</p>
+                        </div>
+
+                        <button className="btn btn-secondary" >Google</button>
+
+                    </div>
+                </div>
+
+            </div>
+            <div className=" hero-content min-w-full flex-col lg:flex-row justify-around">
+            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div className="md:flex">
+                    <div className="md:shrink-0">
+                        
+                        <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://placeimg.com/200/280/arch" alt="Modern building architecture"/>
+                        
+                    </div>
+                    <div className="p-8">
+                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Software Developer</div>
+                        <span className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Bryan Armando Quisaguano Casa</span>
+                        <p className="mt-2 text-slate-500">Apasionado por el backend con java eaaaa y loas mejore sparcticas de programcaion.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div className="md:flex">
+                    <div className="md:shrink-0">
+                        
+                        <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://placeimg.com/200/280/arch" alt="Modern building architecture"/>
+                        
+                    </div>
+                    <div className="p-8">
+                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+                        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accomodation for your team</a>
+                        <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+                    </div>
+                </div>
+            </div>
+           
+            </div>
+            
+        </div>
+     );
+}
+ 
+export default AboutComp;
