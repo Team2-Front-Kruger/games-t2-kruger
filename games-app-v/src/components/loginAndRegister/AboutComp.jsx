15939eb41373a71592384b70735c5bc9011391b5
BryanArmando <66704761+BryanArmando@users.noreply.github.com>
2023-01-15 22:34:01 -0500

    Update about description

--- games-app-v/src/components/loginAndRegister/AboutComp.jsx
+++ games-app-v/src/components/loginAndRegister/AboutComp.jsx
@@ -1,98 +1,134 @@
+import { RiFacebookLine, RiTwitterLine, RiLinkedinLine,RiGithubLine } from "react-icons/ri";
 const AboutComp = () => {
-    return ( 
-        <div>
-            <label className="swap swap-rotate">
-                <input type="checkbox" />
-                <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
-                <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
-            </label>
-
-            <div className="hero-content min-w-full flex-col lg:flex-row w-full justify-around">
-
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#202430]">
-                <figure><img className="card" src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
-                    
-                    <div className="card-body text-white">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Software Developer</div>
-                    <span className="block mt-1 text-lg leading-tight font-medium text-white hover:underline">Bryan Armando Quisaguano Casa</span>
-
-
-                        <div className="text-center">
-                        <p className="mt-2 text-slate-500">Apasionado por el backend con java eaaaa y loas mejore sparcticas de programcación.</p>
-                        </div>
-
-                        <button className="btn btn-secondary" >Google</button>
-
-                    </div>
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#202430]">
-                <figure><img className="card" src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
-                    
-                    <div className="card-body text-white">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Software Developer</div>
-                    <span className="block mt-1 text-lg leading-tight font-medium text-white hover:underline">Bryan Armando Quisaguano Casa</span>
-
-
-                        <div className="text-center">
-                        <p className="mt-2 text-slate-500">Apasionado por el backend con java eaaaa y loas mejore sparcticas de programcación.</p>
-                        </div>
-
-                        <button className="btn btn-secondary" >Google</button>
-
-                    </div>
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#202430]">
-                <figure><img className="card" src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
-                    
-                    <div className="card-body text-white">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Software Developer</div>
-                    <span className="block mt-1 text-lg leading-tight font-medium text-white hover:underline">Bryan Armando Quisaguano Casa</span>
-
-
-                        <div className="text-center">
-                        <p className="mt-2 text-slate-500">Apasionado por el backend con java eaaaa y loas mejore sparcticas de programcación.</p>
-                        </div>
-
-                        <button className="btn btn-secondary" >Google</button>
-
-                    </div>
-                </div>
-
-            </div>
-            <div className=" hero-content min-w-full flex-col lg:flex-row justify-around">
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-                <div className="md:flex">
-                    <div className="md:shrink-0">
-                        
-                        <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://placeimg.com/200/280/arch" alt="Modern building architecture"/>
-                        
-                    </div>
-                    <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Software Developer</div>
-                        <span className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Bryan Armando Quisaguano Casa</span>
-                        <p className="mt-2 text-slate-500">Apasionado por el backend con java eaaaa y loas mejore sparcticas de programcaion.</p>
-                    </div>
-                </div>
-            </div>
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-                <div className="md:flex">
-                    <div className="md:shrink-0">
-                        
-                        <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://placeimg.com/200/280/arch" alt="Modern building architecture"/>
-                        
-                    </div>
-                    <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
-                        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accomodation for your team</a>
-                        <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
-                    </div>
-                </div>
-            </div>
-           
-            </div>
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
             
+            <div className="bg-white hover:bg-cyan-600 group rounded-lg shadow hover:shadow-lg hover:shadow-cyan-700 transition-all hover:cursor-pointer">
+                <div className="flex justify-center py-8">
+                    <img
+                        src="https://pps.whatsapp.net/v/t61.24694-24/294775826_444050540952095_4645322085065350241_n.jpg?ccb=11-4&oh=01_AdRqtrW6UA3Li9DNA1-lwH4s5-w3Fgsb92Ym-PnbYkD2sQ&oe=63D1D717"
+                        alt="bryan"
+                        lazyload="true"
+                        className="rounded-full w-32 h-32 object-cover ring-4 ring-gray-300"
+                    />
+                </div>
+                <div className="flex flex-col items-center gap-2 p-4">
+                    <h3 className="font-semibold text-xl group-hover:text-white transition-all">
+                        Bryan Quisaguano
+                    </h3>
+                    <p className="text-gray-600 group-hover:text-gray-300">
+                        Desarrollador FullStack "React-Spring" 
+                    </p>
+                    <div className="flex items-center">
+                        <a
+                            href="https://www.linkedin.com/in/bryan-quisaguano/"
+                            target="_blank"
+                            className="p-2 hover:bg-gray-100 hover:rounded-lg transition-all"
+                        >
+                            <RiLinkedinLine/>
+                            
+                        </a>
+                        <a
+                            href="https://twitter.com/BryanAr85746106"
+                            target="_blank"
+                            className="p-2 hover:bg-gray-100 hover:rounded-lg transition-all"
+                        >
+                            <RiTwitterLine />
+                        </a>
+                        <a
+                            href="https://github.com/BryanArmando"
+                            target="_blank"
+                            className="p-2 hover:bg-gray-100 hover:rounded-lg transition-all"
+                        >
+                            <RiGithubLine />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-white hover:bg-cyan-600 group rounded-lg shadow hover:shadow-lg hover:shadow-cyan-700 transition-all hover:cursor-pointer">
+                <div className="flex justify-center py-8">
+                    <img
+                        src="https://pps.whatsapp.net/v/t61.24694-24/315366406_131828406188151_4693699854617595623_n.jpg?ccb=11-4&oh=01_AdShF_Eudss7hJFSOLYntoDHiFICSI7lCMCnlyTM-jj50g&oe=63D1DF8C"
+                        alt="JT Devs"
+                        lazyload="true"
+                        className="rounded-full w-32 h-32 object-cover ring-4 ring-gray-300"
+                    />
+                </div>
+                <div className="flex flex-col items-center gap-2 p-4">
+                    <h3 className="font-semibold text-xl group-hover:text-white transition-all">
+                        Javier Yanez
+                    </h3>
+                    <p className="text-gray-600 group-hover:text-gray-300">
+                        Desarrollador FullStack "React-Spring" 
+                    </p>
+                    <div className="flex items-center">
+                        <a
+                            href="https://www.linkedin.com/in/javier-yanez-st/"
+                            target="_blank"
+                            className="p-2 hover:bg-gray-100 hover:rounded-lg transition-all"
+                        >
+                            <RiLinkedinLine />
+                        </a>
+                        <a
+                            href="https://twitter.com/azraedd"
+                            target="_blank"
+                            className="p-2 hover:bg-gray-100 hover:rounded-lg transition-all"
+                        >
+                            <RiTwitterLine />
+                        </a>
+                        <a
+                            href="https://github.com/eddyjav"
+                            target="_blank"
+                            className="p-2 hover:bg-gray-100 hover:rounded-lg transition-all"
+                        >
+                            <RiGithubLine />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-white hover:bg-cyan-600 group rounded-lg shadow hover:shadow-lg hover:shadow-cyan-700 transition-all hover:cursor-pointer">
+                <div className="flex justify-center py-8">
+                    <img
+                        src="https://pps.whatsapp.net/v/t61.24694-24/322450307_534424128653099_8420749515188157528_n.jpg?ccb=11-4&oh=01_AdQsInbzrE07FroEzXEAbWErXWj43nAsaMq60v75n1D21w&oe=63D1BF1D"
+                        alt="JT Devs"
+                        lazyload="true"
+                        className="rounded-full w-32 h-32 object-cover ring-4 ring-gray-300"
+                    />
+                </div>
+                <div className="flex flex-col items-center gap-2 p-4">
+                    <h3 className="font-semibold text-xl group-hover:text-white transition-all">
+                        Luis Anrrango
+                    </h3>
+                    <p className="text-gray-600 group-hover:text-gray-300">
+                        Desarrollador FullStack "React-Spring" 
+                    </p>
+                    <div className="flex items-center">
+                        <a
+                            href="https://www.facebook.com/luis.rigoberto.750"
+                            target="_blank"
+                            className="p-2 hover:bg-gray-100 hover:rounded-lg transition-all"
+                        >
+                            <RiFacebookLine />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/luis-anrrango-a8a25a133"
+                            target="_blank"
+                            className="p-2 hover:bg-gray-100 hover:rounded-lg transition-all"
+                        >
+                            <RiLinkedinLine />
+                        </a>
+                        <a
+                            href="https://github.com/LuisRAnrrango"
+                            target="_blank"
+                            className="p-2 hover:bg-gray-100 hover:rounded-lg transition-all"
+                        >
+                            <RiGithubLine />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default AboutComp;
